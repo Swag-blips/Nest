31f0eae9d71de9cab97e0d8b839908f30e116c65
Swag-blips <coderblip@gmail.com>
2025-05-06 20:51:22 +0100

    feat:hello world nestjs

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HelloService } from './hello.service';
 
+// express ->
 @Controller('hello')
-export class HelloController {}
+export class HelloController {
+  constructor(private readonly helloService: HelloService) {}
+
+  @Get()
+  getHello(): string {
+    return this.helloService.gethello();
+  }
+}
