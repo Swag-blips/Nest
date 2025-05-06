a3099dcb410fa658539631aa62228b4a8f3a77be
Swag-blips <coderblip@gmail.com>
2025-05-06 22:29:18 +0100

    feat:setup user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { HelloService } from 'src/hello/hello.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly helloService: HelloService) {}
+
+  getAllUsers() {
+    return [
+      {
+        id: 1,
+        name: 'Sangam',
+      },
+      {
+        id: 2,
+        name: 'John',
+      },
+      {
+        id: 3,
+        name: 'Victor',
+      },
+    ];
+  }
+
+  getUserById(id: number) {
+    const user = this.getAllUsers().find((user) => user.id === id);
+    return user;
+  }
+}
